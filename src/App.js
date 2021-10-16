90d4060487cb230bfbbaeb082d725c556b0e5a30
Andreis Purim <AndreisPurim@gmail.com>
2021-10-16 18:00:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+
+
+import TitleToolbar from './TitleToolbar.js';
+import Switches from './Switches.js'
+
+
+export default function App() {
+    return (
+        <>
+
+            <TitleToolbar />
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                style={{padding:'2rem'}}
+                >
+                    <Switches />
+            </Grid>
+        </>
+    )
+}
---
+++ src/Switches.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Switch from '@material-ui/core/Switch';
+
+export default function SwitchesGroup() {
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Facebook</FormLabel>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={state.gilad} onChange={handleChange} name="gilad" />}
+          label="Email"
+        />
+        <FormControlLabel
+          control={<Switch checked={state.jason} onChange={handleChange} name="jason" />}
+          label="Geolocation"
+        />
+        <FormControlLabel
+          control={<Switch checked={state.antoine} onChange={handleChange} name="antoine" />}
+          label="Photo Galery"
+        />
+      </FormGroup>
+      <FormHelperText>Be careful with your data!</FormHelperText>
+    </FormControl>
+  );
+}
